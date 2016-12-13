951d78388b7129eb974eb222ef914a276087544d
Tim <tbrandle53@gmail.com>
2016-12-12 17:44:51 -0700

    add jQuery append functionality.  It will now append a new website every time user clicks enter-btn

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 //to add the bookmark, use jQuery .append(.article)
 
 
-$('#enter-btn').on('click', function(){
-  $('.article-div1 h2').text($('#bm-title').val());
-  $('#article').clone().appendTo('#web-list-section');
-
-})
+$('#enter-btn').on('click', function (){
+  var $title = $('#bm-title').val();
+  var $url = $('#bm-url').val();
+  $('#web-list-section').append(
+      `<article id="article">
+          <div class="article-div1">
+            <h2>${$title}</h2>
+          </div>
+          <div class="article-div2">
+            <a href="">${$url}</a>
+          </div>
+          <div class="article-div3">
+            <button class="read-btn" type="button" name="read">Read</button>
+            <button class="delete-btn" type="button" name="delete">Delete</button>
+          </div>
+        </article>`)
+  }
+)
