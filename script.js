2500b248c0a02d10a719c92f9aff0403ced22fb4
Tim <tbrandle53@gmail.com>
2016-12-12 19:16:03 -0700

    toggle class to read btn

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 //to add the bookmark, use jQuery .append(.article)
-
-
 $('#enter-btn').on('click', function (){
   var $title = $('#bm-title').val();
   var $url = $('#bm-url').val();
@@ -12,10 +10,16 @@ $('#enter-btn').on('click', function (){
           <div class="article-div2">
             <a href="">${$url}</a>
           </div>
-          <div class="article-div3">
+
             <button class="read-btn" type="button" name="read">Read</button>
             <button class="delete-btn" type="button" name="delete">Delete</button>
-          </div>
+
         </article>`)
   }
 )
+
+//add toggleClass to the visited
+$('#web-list-section').on('click','.read-btn', function() { //I needed to target the section id that was already on the page and then add add a target
+  $(this).toggleClass('read');
+  // $(this).parent().toggleClass('visited-article');
+})
